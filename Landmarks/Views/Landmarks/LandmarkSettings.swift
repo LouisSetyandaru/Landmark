b2fba39b5926cdc9b7253f9bc4033a37fe9bbb39
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-04-26 08:12:31 +0700

    Commit Final

---
+++ Landmarks/Views/Landmarks/LandmarkSettings.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by student on 26/04/24.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkSettings()
+}
