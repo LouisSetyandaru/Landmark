200f4579fe0c2dc07ca8b44af64b31d75f37166d
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 09:07:24 +0700

    Penambahan Dokumentasi PageViewController

--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 import UIKit
 
-
+// Struct PageViewController bertindak sebagai UIViewControllerRepresentable untuk mengintegrasikan UIPageViewController ke dalam SwiftUI.
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
-    var pages: [Page]
-    @Binding var currentPage: Int
-    
+    var pages: [Page] // Array halaman yang akan ditampilkan di UIPageViewController.
+    @Binding var currentPage: Int // Binding untuk memantau halaman yang sedang ditampilkan.
+
+    // Metode ini membuat coordinator untuk PageViewController.
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
-    
+    // Metode ini membuat instance baru dari UIPageViewController.
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
@@ -29,24 +30,24 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
         return pageViewController
     }
     
-    
+    // Metode ini mengupdate UIPageViewController dengan halaman yang sesuai dengan currentPage.
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
             [context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
     }
     
-    
+    // Kelas Coordinator bertindak sebagai delegate untuk UIPageViewController.
     class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
-        var controllers = [UIViewController]()
-        
+        var controllers = [UIViewController]() // Array untuk menyimpan UIHostingController dari setiap halaman.
         
         init(_ pageViewController: PageViewController) {
             parent = pageViewController
+            // Menginisialisasi UIHostingController untuk setiap halaman yang diberikan.
             controllers = parent.pages.map { UIHostingController(rootView: $0) }
         }
         
-        
+        // Metode ini mengembalikan halaman sebelumnya dalam urutan halaman.
         func pageViewController(
             _ pageViewController: UIPageViewController,
             viewControllerBefore viewController: UIViewController) -> UIViewController?
@@ -60,7 +61,7 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             return controllers[index - 1]
         }
         
-        
+        // Metode ini mengembalikan halaman setelahnya dalam urutan halaman.
         func pageViewController(
             _ pageViewController: UIPageViewController,
             viewControllerAfter viewController: UIViewController) -> UIViewController?
@@ -74,7 +75,7 @@ struct PageViewController<Page: View>: UIViewControllerRepresentable {
             return controllers[index + 1]
         }
         
-        
+        // Metode ini dipanggil setelah transisi selesai dan memperbarui currentPage jika diperlukan.
         func pageViewController(
             _ pageViewController: UIPageViewController,
             didFinishAnimating finished: Bool,
