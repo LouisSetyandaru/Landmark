f67967d32ba057c362d647eb3001d6fa822acf50
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 09:20:20 +0700

    Penambahan Dokumentasi ProfileHost

--- Landmarks/Views/Profiles/ProfileHost.swift
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
+// Struct ProfileHost bertindak sebagai tampilan utama untuk menampilkan profil pengguna.
 struct ProfileHost: View {
-    @Environment(\.editMode) var editMode
-    @Environment(ModelData.self) var modelData
-    @State private var draftProfile = Profile.default
+    @Environment(\.editMode) var editMode // Environment untuk mengakses mode pengeditan.
+    @Environment(ModelData.self) var modelData // EnvironmentObject untuk mengakses data model.
+    @State private var draftProfile = Profile.default // State untuk menyimpan draft profil yang diedit.
 
 
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             HStack {
+                // Tombol "Cancel" untuk membatalkan pengeditan profil.
                 if editMode?.wrappedValue == .active {
                     Button("Cancel", role: .cancel) {
                         draftProfile = modelData.profile
@@ -23,10 +25,11 @@ struct ProfileHost: View {
                     }
                 }
                 Spacer()
+                // Tombol "Edit" untuk memulai atau mengakhiri mode pengeditan.
                 EditButton()
             }
 
-
+            // Menampilkan ProfileSummary jika editMode tidak aktif, atau ProfileEditor jika sedang dalam mode pengeditan.
             if editMode?.wrappedValue == .inactive {
                 ProfileSummary(profile: modelData.profile)
             } else {
