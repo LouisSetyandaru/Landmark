54658929435dee48f7a77745c3c6ccc8349671a4
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 10:13:45 +0700

    Penambahan Dokumentasi FavouriteButton

--- Landmarks/Views/Helper/FavoriteButton.swift
+++ Landmarks/Views/Helper/FavoriteButton.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 
 struct FavoriteButton: View {
-    @Binding var isSet: Bool
+    @Binding var isSet: Bool // Binding untuk menentukan apakah item favorit atau tidak.
 
 
     var body: some View {
+        // Tombol yang berisi ikon bintang sesuai dengan status item favorit atau tidak.
         Button {
-            isSet.toggle()
+            isSet.toggle() // Mengubah status item favorit.
         } label: {
-            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
-                .labelStyle(.iconOnly)
-                .foregroundStyle(isSet ? .yellow : .gray)
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star") // Label berisi ikon bintang yang sesuai dengan status item favorit.
+                .labelStyle(.iconOnly) // Menetapkan gaya label ke iconOnly.
+                .foregroundStyle(isSet ? .yellow : .gray) // Mengatur warna teks sesuai dengan status item favorit.
         }
     }
 }
