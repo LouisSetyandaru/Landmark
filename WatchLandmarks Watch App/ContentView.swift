0c40bcbae18e3e72337484d62e37e857a7085d95
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 10:23:58 +0700

    Penambahan Dokuentasi ContentView pada WatchOS

--- WatchLandmarks Watch App/ContentView.swift
+++ WatchLandmarks Watch App/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 import UserNotifications
 
-
+// ContentView adalah tampilan utama aplikasi.
 struct ContentView: View {
     var body: some View {
+        // Menampilkan daftar landmark pada tampilan utama.
         LandmarkList()
+        // Menggunakan task untuk meminta izin notifikasi ketika tampilan dimuat.
             .task {
                 let center = UNUserNotificationCenter.current()
+                // Meminta izin untuk menampilkan notifikasi dengan alert, sound, dan badge.
                 _ = try? await center.requestAuthorization(
                     options: [.alert, .sound, .badge]
                 )
