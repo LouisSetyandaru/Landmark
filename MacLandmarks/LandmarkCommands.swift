220d07ef047966475a2f8f8ff0dae3fc55e6da15
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 10:38:29 +0700

    Penambahan Dokumentasi LandmarkCommands pada MacOS

--- MacLandmarks/LandmarkCommands.swift
+++ MacLandmarks/LandmarkCommands.swift
@@ -7,30 +7,35 @@
 
 import SwiftUI
 
-
+// Struktur untuk menentukan perintah yang berlaku untuk landmark.
 struct LandmarkCommands: Commands {
+    // Binding yang fokus pada landmark yang dipilih.
     @FocusedBinding(\.selectedLandmark) var selectedLandmark
 
 
     var body: some Commands {
+        // Menambahkan perintah sidebar.
         SidebarCommands()
-        
+        // Menu perintah untuk landmark.
         CommandMenu("Landmark") {
+            // Tombol untuk menandai atau menghapus landmark sebagai favorit.
             Button("\(selectedLandmark?.isFavorite == true ? "Remove" : "Mark") as Favorite") {
                 selectedLandmark?.isFavorite.toggle()
             }
+            // Shortcut keyboard untuk menandai landmark sebagai favorit.
             .keyboardShortcut("f", modifiers: [.shift, .option])
+            // Menonaktifkan tombol jika tidak ada landmark yang dipilih.
             .disabled(selectedLandmark == nil)
         }
     }
 }
 
-
+// Kunci Binding yang fokus pada landmark yang dipilih.
 private struct SelectedLandmarkKey: FocusedValueKey {
     typealias Value = Binding<Landmark>
 }
 
-
+// Ekstensi untuk mendapatkan atau mengatur Binding yang fokus pada landmark yang dipilih.
 extension FocusedValues {
     var selectedLandmark: Binding<Landmark>? {
         get { self[SelectedLandmarkKey.self] }
