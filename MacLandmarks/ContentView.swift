da6757cb781f067113924dac6c44d799bd4fccc0
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 10:33:32 +0700

    Penambahan Dokuemtasi ContentView pada MacOS

--- MacLandmarks/ContentView.swift
+++ MacLandmarks/ContentView.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 
+// Tampilan konten utama yang menampilkan daftar landmark.
 struct ContentView: View {
     var body: some View {
         LandmarkList()
+        // Memuat tampilan daftar landmark dengan ukuran minimum.
             .frame(minWidth: 700, minHeight: 300)
     }
 }
