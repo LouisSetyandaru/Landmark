67152a261e1c4357732fe22cddbb7b32749034a3
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 09:39:05 +0700

    Penambahan Dokumentasi GraphCapsule

--- Landmarks/Views/Hikes/GraphCapsule.swift
+++ Landmarks/Views/Hikes/GraphCapsule.swift
@@ -6,23 +6,25 @@ A single line in the graph.
 */
 
 import SwiftUI
-
+// Struct GraphCapsule mewakili satu baris dalam grafik.
 struct GraphCapsule: View, Equatable {
-    var index: Int
+    var index: Int // Indeks baris dalam grafik.
     var color: Color
-    var height: CGFloat
-    var range: Range<Double>
-    var overallRange: Range<Double>
+    var height: CGFloat // Tinggi total grafik.
+    var range: Range<Double> // Rentang nilai yang direpresentasikan oleh baris.
+    var overallRange: Range<Double> // Rentang nilai keseluruhan dalam grafik.
 
+    // Perbandingan tinggi baris terhadap tinggi total grafik.
     var heightRatio: CGFloat {
         max(CGFloat(magnitude(of: range) / magnitude(of: overallRange)), 0.15)
     }
-
+    // Perbandingan offset vertikal baris terhadap tinggi total grafik.
     var offsetRatio: CGFloat {
         CGFloat((range.lowerBound - overallRange.lowerBound) / magnitude(of: overallRange))
     }
 
     var body: some View {
+        // Menampilkan Capsule dengan warna, tinggi, dan posisi yang sesuai.
         Capsule()
             .fill(color)
             .frame(height: height * heightRatio)
