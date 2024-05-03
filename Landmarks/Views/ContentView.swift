0a6ebc46ad09ac2e13ff93edadc795b235b53e7b
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 09:00:18 +0700

    Penambahan Dokumentasi ContentView

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,23 +9,26 @@ import SwiftUI
 
 // Struct yang mewakili tampilan utama aplikasi
 struct ContentView: View {
+    //State yang menyimpan tab yang sedang di pilih
     @State private var selection: Tab = .featured
     
-    
+    //Enum untuk dua tab yang tersedia: pada 'feature' dan 'list'.
     enum Tab {
         case featured
         case list
     }
     
     var body: some View {
+        //TabView untuk tata letak yang menampilkan berberapa tampilan dalam tab
         TabView(selection: $selection) {
+            // Tampilan CategoryHome ditampilkan di tab 'Featured'.
             CategoryHome()
                 .tabItem {
                     Label("Featured", systemImage: "star")
                 }
                 .tag(Tab.featured)
             
-            
+            // Tampilan LandmarkList ditampilkan di tab 'List'.
             LandmarkList()
                 .tabItem {
                     Label("List", systemImage: "list.bullet")
