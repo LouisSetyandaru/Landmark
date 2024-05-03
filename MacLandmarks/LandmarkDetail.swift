be441c1acfb28757fbccbdd657c5b203d37dfef3
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 10:36:38 +0700

    Penmabahan Dokumentasi LandmarkDetail pada MacOS

--- MacLandmarks/LandmarkDetail.swift
+++ MacLandmarks/LandmarkDetail.swift
@@ -10,24 +10,27 @@ import MapKit
 
 
 struct LandmarkDetail: View {
+    // Lingkungan model data untuk mengakses data landmark.
     @Environment(ModelData.self) var modelData
+    // Data landmark yang ditampilkan.
     var landmark: Landmark
-
-
+    // Indeks landmark dalam data model.
     var landmarkIndex: Int {
         modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
     }
 
 
     var body: some View {
+        // Mengikat model data untuk penggunaan dalam tampilan.
         @Bindable var modelData = modelData
         
         ScrollView {
             ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
+                // Tampilan peta landmark.
                 MapView(coordinate: landmark.locationCoordinate)
                     .frame(height: 300)
 
-
+                // Tombol untuk membuka peta dalam aplikasi Peta.
                 Button("Open in Maps") {
                     let destination = MKMapItem(placemark: MKPlacemark(coordinate: landmark.locationCoordinate))
                     destination.name = landmark.name
@@ -47,6 +50,7 @@ struct LandmarkDetail: View {
                         HStack {
                             Text(landmark.name)
                                 .font(.title)
+                            // Tombol untuk menandai landmark sebagai favorit.
                             FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
                                 .buttonStyle(.plain)
                         }
