ac007479d9cfb9b17f64804f38a2323008ee05b7
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 10:07:27 +0700

    Penambahan Dokumentasi BadgesBackground

--- Landmarks/Views/Badges/BadgeBackground.swift
+++ Landmarks/Views/Badges/BadgeBackground.swift
@@ -7,24 +7,24 @@
 
 import SwiftUI
 
-
+// Struct BadgeBackground untuk menampilkan latar belakang badge.
 struct BadgeBackground: View {
     var body: some View {
-        GeometryReader { geometry in
-            Path { path in
+        GeometryReader { geometry in // Membaca geometri dari tata letak.
+            Path { path in // Membuat path untuk latar belakang badge.
                 var width: CGFloat = min(geometry.size.width, geometry.size.height)
                 let height = width
                 let xScale: CGFloat = 0.832
                 let xOffset = (width * (1.0 - xScale)) / 2.0
                 width *= xScale
-                path.move(
+                path.move( // Memindahkan path ke titik awal.
                     to: CGPoint(
                         x: width * 0.95 + xOffset,
                         y: height * (0.20 + HexagonParameters.adjustment)
                     )
                 )
 
-
+                // Menambahkan garis dan kurva pada path sesuai parameter dari HexagonParameters.
                 HexagonParameters.segments.forEach { segment in
                     path.addLine(
                         to: CGPoint(
@@ -46,14 +46,15 @@ struct BadgeBackground: View {
                     )
                 }
             }
-            .fill(.linearGradient(
+            .fill(.linearGradient( // Mengisi path dengan gradien linear.
                 Gradient(colors: [Self.gradientStart, Self.gradientEnd]),
-                startPoint: UnitPoint(x: 0.5, y: 0),
-                endPoint: UnitPoint(x: 0.5, y: 0.6)
+                startPoint: UnitPoint(x: 0.5, y: 0), // Titik awal gradien.
+                endPoint: UnitPoint(x: 0.5, y: 0.6) // Titik akhir gradien.
             ))
         }
-        .aspectRatio(1, contentMode: .fit)
+        .aspectRatio(1, contentMode: .fit) // Menyesuaikan aspek rasio tata letak.
     }
+    // Warna awal dan akhir gradien untuk latar belakang badge.
     static let gradientStart = Color(red: 239.0 / 255, green: 120.0 / 255, blue: 221.0 / 255)
     static let gradientEnd = Color(red: 239.0 / 255, green: 172.0 / 255, blue: 120.0 / 255)
 }
