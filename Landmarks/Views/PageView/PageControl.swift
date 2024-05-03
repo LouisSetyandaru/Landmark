cd7ac400c66d36d6d5ebe016804d10a2964dec3b
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 09:16:00 +0700

    Penambahan Dokumentasi PageControll

--- Landmarks/Views/PageView/PageControl.swift
+++ Landmarks/Views/PageView/PageControl.swift
@@ -8,34 +8,34 @@
 import SwiftUI
 import UIKit
 
+// Struct PageControl adalah UIViewRepresentable untuk menampilkan UIPageControl dalam SwiftUI.
 struct PageControl: UIViewRepresentable {
-    var numberOfPages: Int
-    @Binding var currentPage: Int
+    var numberOfPages: Int // Jumlah total halaman.
+    @Binding var currentPage: Int // Binding untuk memantau halaman yang sedang ditampilkan.
     
-    
+    // Metode ini membuat coordinator untuk PageControl.
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
-    
+    // Metode ini membuat instance baru dari UIPageControl.
     func makeUIView(context: Context) -> UIPageControl {
         let control = UIPageControl()
-        control.numberOfPages = numberOfPages
+        control.numberOfPages = numberOfPages // Set jumlah total halaman.
         control.addTarget(
             context.coordinator,
             action: #selector(Coordinator.updateCurrentPage(sender:)),
-            for: .valueChanged)
-        
+            for: .valueChanged)// Menambahkan target dan aksi untuk memperbarui halaman saat UIPageControl berubah.
         
         return control
     }
     
-    
+    // Metode ini mengupdate tampilan UIPageControl dengan halaman yang sesuai dengan currentPage.
     func updateUIView(_ uiView: UIPageControl, context: Context) {
         uiView.currentPage = currentPage
     }
     
-    
+    // Kelas Coordinator bertindak sebagai delegate untuk UIPageControl.
     class Coordinator: NSObject {
         var control: PageControl
         
@@ -44,7 +44,7 @@ struct PageControl: UIViewRepresentable {
             self.control = control
         }
         
-        
+        // Metode ini dipanggil ketika halaman yang ditampilkan dalam UIPageControl berubah.
         @objc
         func updateCurrentPage(sender: UIPageControl) {
             control.currentPage = sender.currentPage
