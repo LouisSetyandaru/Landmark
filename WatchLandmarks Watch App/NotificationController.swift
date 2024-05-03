ffe8463dc595080f01421640ffe7c0027e0a3b20
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 10:30:34 +0700

    Penambahan Dokumentasi NotificationController pada WatchOS

--- WatchLandmarks Watch App/NotificationController.swift
+++ WatchLandmarks Watch App/NotificationController.swift
@@ -9,14 +9,16 @@ import WatchKit
 import SwiftUI
 import UserNotifications
 
-
+// Pengontrol untuk menangani notifikasi dan menampilkan tampilan notifikasi.
 class NotificationController: WKUserNotificationHostingController<NotificationView> {
     var landmark: Landmark?
     var title: String?
     var message: String?
     
+    // Kunci untuk mengambil indeks landmark dari informasi notifikasi.
     let landmarkIndexKey = "landmarkIndex"
     
+    // Menampilkan tampilan notifikasi dengan judul, pesan, dan informasi landmark yang diterima.
     override var body: NotificationView {
         NotificationView(
             title: title,
@@ -25,15 +27,15 @@ class NotificationController: WKUserNotificationHostingController<NotificationVi
         )
     }
 
-
+    // Menangani penerimaan notifikasi.
     override func didReceive(_ notification: UNNotification) {
         let modelData = ModelData()
 
-
+        // Mendapatkan data notifikasi.
         let notificationData =
             notification.request.content.userInfo as? [String: Any]
 
-
+        // Mendapatkan informasi judul dan pesan dari notifikasi.
         let aps = notificationData?["aps"] as? [String: Any]
         let alert = aps?["alert"] as? [String: Any]
 
@@ -41,7 +43,7 @@ class NotificationController: WKUserNotificationHostingController<NotificationVi
         title = alert?["title"] as? String
         message = alert?["body"] as? String
 
-
+        // Mendapatkan indeks landmark jika ada dari informasi notifikasi.
         if let index = notificationData?[landmarkIndexKey] as? Int {
             landmark = modelData.landmarks[index]
         }
