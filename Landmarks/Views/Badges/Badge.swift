dbd4ca2812967fbf76652a295778f21a799b23a1
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 10:09:58 +0700

    Penmabahan Dokumentasi Badge

--- Landmarks/Views/Badges/Badge.swift
+++ Landmarks/Views/Badges/Badge.swift
@@ -7,28 +7,29 @@
 
 import SwiftUI
 
-
+// Struct Badge untuk menampilkan badge dengan simbol badge.
 struct Badge: View {
+    // Variabel computed untuk mendefinisikan simbol badge.
     var badgeSymbols: some View {
-        ForEach(0..<8) { index in
-            RotatedBadgeSymbol(
-                angle: .degrees(Double(index) / Double(8)) * 360.0
+        ForEach(0..<8) { index in // Perulangan untuk menampilkan 8 simbol badge yang diputar.
+            RotatedBadgeSymbol( // Menggunakan RotatedBadgeSymbol untuk menampilkan simbol badge yang diputar.
+                angle: .degrees(Double(index) / Double(8)) * 360.0 // Menghitung sudut rotasi untuk setiap simbol.
             )
         }
-        .opacity(0.5)
+        .opacity(0.5) // Menetapkan opacity untuk simbol badge.
     }
     
     var body: some View {
-        ZStack {
-            BadgeBackground()
+        ZStack { // Menumpuk BadgeBackground dan simbol badge.
+            BadgeBackground() // Menampilkan latar belakang badge.
             
-            GeometryReader { geometry in
+            GeometryReader { geometry in // Membaca geometri dari tata letak.
                 badgeSymbols
-                    .scaleEffect(1.0 / 4.0, anchor: .top)
-                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+                    .scaleEffect(1.0 / 4.0, anchor: .top) // Menyesuaikan skala simbol badge.
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height) // Menempatkan simbol badge di tengah.
             }
         }
-        .scaledToFit()
+        .scaledToFit() // Menyesuaikan tata letak ke ukuran konten.
     }
 }
 
