2ed846bb507d2e28114831ce8ffaff15632c427a
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 09:49:40 +0700

    Penambahan Dokumentasi HikeBadge

--- Landmarks/Views/Hikes/HikeBadge.swift
+++ Landmarks/Views/Hikes/HikeBadge.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
+// Struct HikeBadge menampilkan badge untuk suatu hike.
 struct HikeBadge: View {
-    var name: String
+    var name: String // Nama badge.
 
 
     var body: some View {
         VStack(alignment: .center) {
-            Badge()
+            Badge() // Menampilkan badge.
                 .frame(width: 300, height: 300)
                 .scaleEffect(1.0 / 3.0)
                 .frame(width: 100, height: 100)
