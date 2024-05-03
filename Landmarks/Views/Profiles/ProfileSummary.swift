772944e4d9066c1697d351af16d3a9c8f4e001f0
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 09:24:24 +0700

    Penambahan Dokumentasi ProfileSummary

--- Landmarks/Views/Profiles/ProfileSummary.swift
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -7,32 +7,35 @@
 
 import SwiftUI
 
+// Struct ProfileSummary menampilkan ringkasan dari profil pengguna.
 struct ProfileSummary: View {
-    @Environment(ModelData.self) var modelData
-    var profile: Profile
+    @Environment(ModelData.self) var modelData // EnvironmentObject untuk mengakses data model.
+    var profile: Profile // Profil pengguna yang akan ditampilkan.
     
     
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 10) {
+                // Menampilkan nama pengguna dengan format tebal dan ukuran font besar.
                 Text(profile.username)
                     .bold()
                     .font(.title)
-                
-                
+                // Menampilkan status notifikasi pengguna.
                 Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
+                // Menampilkan preferensi foto musiman pengguna.
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                // Menampilkan tanggal target pencapaian pengguna.
                 Text("Goal Date: ") + Text(profile.goalDate, style: .date)
                 
-                
+                // Pembatas antara informasi profil dan badge hiking.
                 Divider()
                 
-                
+                // Menampilkan badge hiking yang telah dicapai pengguna.
                 VStack(alignment: .leading) {
                     Text("Completed Badges")
                         .font(.headline)
                     
-                    
+                    // ScrollView horizontal untuk badge hiking.
                     ScrollView(.horizontal) {
                         HStack {
                             HikeBadge(name: "First Hike")
@@ -46,15 +49,15 @@ struct ProfileSummary: View {
                     }
                 }
                 
-                
+                // Pembatas antara badge hiking dan informasi hike terbaru.
                 Divider()
                 
-                
+                // Menampilkan informasi tentang hike terbaru pengguna.
                 VStack(alignment: .leading) {
                     Text("Recent Hikes")
                         .font(.headline)
                     
-                    
+                    // Menampilkan tampilan hike terbaru.
                     HikeView(hike: modelData.hikes[0])
                 }
             }
