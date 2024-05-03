36e402569416c72290a0198c2d85187681f1f8e0
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 09:53:14 +0700

    Penambahan Dokumentasi LandmarkList

--- Landmarks/Views/Landmarks/LandmarkList.swift
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -7,12 +7,14 @@
 
 import SwiftUI
 
+// Struct LandmarkList menampilkan daftar landmark.
 struct LandmarkList: View {
-    @Environment(ModelData.self) var modelData
-    @State private var showFavoritesOnly = false
-    @State private var filter = FilterCategory.all
-    @State private var selectedLandmark: Landmark?
+    @Environment(ModelData.self) var modelData // Data model yang digunakan.
+    @State private var showFavoritesOnly = false // State untuk menunjukkan hanya tampilkan favorit atau tidak.
+    @State private var filter = FilterCategory.all // State untuk filter kategori landmark.
+    @State private var selectedLandmark: Landmark? // State untuk landmark yang dipilih.
     
+    // Enum untuk kategori filter.
     enum FilterCategory: String, CaseIterable, Identifiable {
         case all = "All"
         case lakes = "Lakes"
@@ -23,7 +25,7 @@ struct LandmarkList: View {
         var id: FilterCategory { self }
     }
     
-    
+    // Mengembalikan array landmark yang sudah difilter.
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
@@ -31,16 +33,18 @@ struct LandmarkList: View {
         }
     }
     
+    // Judul daftar landmark yang ditampilkan.
     var title: String {
         let title = filter == .all ? "Landmarks" : filter.rawValue
         return showFavoritesOnly ? "Favorite \(title)" : title
     }
     
+    // Index dari landmark yang dipilih.
     var index: Int? {
         modelData.landmarks.firstIndex(where: { $0.id == selectedLandmark?.id })
     }
     
-    
+    // Tampilan utama daftar landmark dengan NavigationSplitView.
     var body: some View {
         @Bindable var modelData = modelData
         
@@ -60,6 +64,7 @@ struct LandmarkList: View {
             .frame(minWidth: 300)
             .toolbar {
                 ToolbarItem {
+                    // Menu untuk filter kategori dan pilihan hanya favorit.
                     Menu {
                         Picker("Category", selection: $filter) {
                             ForEach(FilterCategory.allCases) { category in
