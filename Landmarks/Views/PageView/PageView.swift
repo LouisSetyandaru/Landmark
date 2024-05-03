e51622c78eb0bec6b2b0d631af7d2cf5aeb72150
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 09:12:11 +0700

    Penambahan Dokumentasi PageView

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -6,20 +6,23 @@
 //
 
 import SwiftUI
-
+// Struct PageView adalah tampilan yang menampilkan halaman-halaman dalam UIPageViewController bersama dengan PageControl.
 struct PageView<Page: View>: View {
-    var pages: [Page]
-    @State private var currentPage = 1
+    var pages: [Page] // Array halaman yang akan ditampilkan.
+    @State private var currentPage = 1 // State untuk memantau halaman yang sedang ditampilkan.
     
     
     var body: some View {
+        // Tampilan utama PageView, menampilkan PageViewController dan PageControl.
         ZStack(alignment: .bottomTrailing) {
+            // PageViewController menampilkan halaman-halaman dari array.
             PageViewController(pages: pages, currentPage: $currentPage)
+            // PageControl menampilkan indikator halaman.
             PageControl(numberOfPages: pages.count, currentPage: $currentPage)
                 .frame(width: CGFloat(pages.count * 18))
                 .padding(.trailing)
         }
-        .aspectRatio(3 / 2, contentMode: .fit)
+        .aspectRatio(3 / 2, contentMode: .fit) // Aspek rasio tampilan PageView.
     }
 }
 
