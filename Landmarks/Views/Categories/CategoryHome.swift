164f58340cef185b069eadcefe13758d839e302e
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 09:30:43 +0700

    Penambahan Dokumentasi CategoryHome

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -7,10 +7,10 @@
 
 import SwiftUI
 
-
+// Struct CategoryHome menampilkan tampilan utama aplikasi dengan daftar fitur dan kategori.
 struct CategoryHome: View {
-    @Environment(ModelData.self) var modelData
-    @State private var showingProfile = false
+    @Environment(ModelData.self) var modelData // EnvironmentObject untuk mengakses data model.
+    @State private var showingProfile = false // State untuk menampilkan profil pengguna.
     
     var body: some View {
         NavigationSplitView {
@@ -21,9 +21,11 @@ struct CategoryHome: View {
                 //                    .frame(height: 200)
                 //                    .clipped()
                 //                    .listRowInsets(EdgeInsets())
+                
+                // Menampilkan PageView dengan fitur-fitur utama sebagai halaman.
                 PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
                     .listRowInsets(EdgeInsets())
-                
+                // Menampilkan setiap kategori dan itemnya sebagai CategoryRow.
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
@@ -32,12 +34,14 @@ struct CategoryHome: View {
             .listStyle(.inset)
             .navigationTitle("Featured")
             .toolbar {
+                // Tombol untuk menampilkan profil pengguna.
                 Button {
                     showingProfile.toggle()
                 } label: {
                     Label("User Profile", systemImage: "person.crop.circle")
                 }
             }
+            // Menampilkan ProfileHost dalam sheet ketika showingProfile aktif.
             .sheet(isPresented: $showingProfile) {
                 ProfileHost()
                     .environment(modelData)
