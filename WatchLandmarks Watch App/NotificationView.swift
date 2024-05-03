b30725c5ee7086aa4f4239ea3c77197b31aa49d9
LouisSetyandaru <lsetyandaru@student.ciputra.ac.id>
2024-05-03 10:28:28 +0700

    Penambahan Dokumentasi NotificationView pada WatchOS

--- WatchLandmarks Watch App/NotificationView.swift
+++ WatchLandmarks Watch App/NotificationView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-
+// Tampilan untuk menampilkan notifikasi.
 struct NotificationView: View {
     var title: String?
     var message: String?
@@ -16,31 +16,28 @@ struct NotificationView: View {
 
     var body: some View {
         VStack {
+            // Menampilkan gambar lingkaran landmark jika tersedia.
             if let landmark {
                 CircleImage(image: landmark.image.resizable())
                     .scaledToFit()
             }
-
-
+            // Menampilkan judul notifikasi atau "Unknown Landmark" jika tidak ada judul.
             Text(title ?? "Unknown Landmark")
                 .font(.headline)
-
-
             Divider()
-
-
+            // Menampilkan pesan notifikasi atau pesan default jika tidak ada pesan.
             Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
                 .font(.caption)
         }
     }
 }
 
-
+// Preview untuk tampilan NotificationView tanpa argumen.
 #Preview {
     NotificationView()
 }
 
-
+// Preview untuk tampilan NotificationView dengan argumen tertentu.
 #Preview {
     NotificationView(
         title: "Turtle Rock",
